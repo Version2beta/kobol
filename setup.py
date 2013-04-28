342c01c9b56f37a12700ae1a0c599cd9678a8bc0
Rob Martin <rob@version2beta.com>
2013-04-28 14:37:43 -0500

    Initial setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+
+setup(
+    name='kobol',
+    version='0.0.1',
+    author=['Rob Martin @version2beta', 'Miranda Handley @PhoenixToFlame'],
+    author_email=['rob@version2beta.com', 'miranda@mirandahandley.com'],
+    packages=['kobol'],
+    scripts=[],
+    url='http://pypi.python.org/pypi/kobol/',
+    license='LICENSE.txt',
+    description='A static site generator for Python.',
+    long_description=open('README.rst').read(),
+    install_requires=[
+      'flask',
+      'jinja2',
+      'flask_frozen',
+      'pygments',
+      'boto',
+      'unittest',
+      'expecter',
+      'dingus'
+    ],
+    package_data={
+        '': ['*.dist'],
+      },
+    entry_points={
+        'console_scripts': [
+          'kobol = kobol:main',
+        ],
+      },
+)
